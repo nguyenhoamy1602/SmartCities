c8dd44a3c5225111661724fd07485f1ba471a3f3
David Burela <David.Burela@gmail.com>
2017-10-21 14:25:18 +1100

    added a migration script to create initial data

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,20 @@
 var ConvertLib = artifacts.require("./ConvertLib.sol");
 var MetaCoin = artifacts.require("./MetaCoin.sol");
+var Sensor = artifacts.require("./Sensor.sol");
 
-module.exports = function(deployer) {
+module.exports = function (deployer) {
   deployer.deploy(ConvertLib);
   deployer.link(ConvertLib, MetaCoin);
   deployer.deploy(MetaCoin);
-};
+  deployer.deploy(Sensor, "123,111", "1").then(function (instance) {
+
+    Sensor.deployed().then(function (instance) {
+      var sensor = instance;
+      sensor.storeSensorData("1/1/2017", Math.round(Math.random() * 100));
+      sensor.storeSensorData("1/1/2017", Math.round(Math.random() * 100));
+      sensor.storeSensorData("1/1/2017", Math.round(Math.random() * 100));
+      sensor.storeSensorData("1/1/2017", Math.round(Math.random() * 100));
+      sensor.storeSensorData("1/1/2017", Math.round(Math.random() * 100));
+    });
+  });
+  };
