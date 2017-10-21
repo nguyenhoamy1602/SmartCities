5cfe2d5533cf1998ef695ac5a9ab7c37429a5c00
nguyenhoamy1602 <nguyenhoamy1602@gmail.com>
2017-10-21 13:11:11 +1100

    Initial Commit

---
+++ contracts/Sensor.sol
@@ -0,0 +1,47 @@
+pragma solidity ^0.4.2;
+
+import "./ConvertLib.sol";
+
+// This is just a simple example of a coin-like contract.
+// It is not standards compatible and cannot be expected to talk to other
+// coin/token contracts. If you want to create a standards-compliant
+// token, see: https://github.com/ConsenSys/Tokens. Cheers!
+
+contract Sensor {
+	struct DateTime {
+        uint16 year;
+        uint8 month;
+        uint8 day;
+        uint8 hour;
+        uint8 minute;
+        uint8 second;
+        uint8 weekday;
+	}
+
+	struct Reading {
+		string time;
+		uint value;
+	}
+
+	uint id;
+	Reading[] sensorData;
+	string location;
+
+
+
+	function Sensor(string _location, uint _id) {
+		id = _id;
+		location = _location;
+	}
+
+	function storeSensorData(string _time, uint _value) {
+		sensorData.push(Reading(_time, _value));
+	}
+
+	function readSensorData(uint _x) constant returns(string, uint) {
+		uint lastElement = sensorData.length - 1;
+		return (sensorData[lastElement - _x].time, sensorData[lastElement - _x].value);
+	}
+
+
+}
